60e0529f45c8670d93e2532a7de1048ef78ab192
CedricT33 <cedric.tressous@gmail.com>
2018-10-18 11:46:36 +0200

    ecriture commentaires

--- MasterMind.java
+++ MasterMind.java
@@ -3,6 +3,10 @@ import java.util.Hashtable;
 import java.util.Arrays;
 import java.lang.Thread;
 
+/**
+*Classe principale du jeu MasterMind
+*Codé avec amour par Vincent et Cédric de Simplon !!
+*/
 public class MasterMind {
 
   static Scanner saisie;
@@ -11,23 +15,23 @@ public class MasterMind {
 
     saisie = new Scanner(System.in);
 
-    // Jaune, Rouge, Vert, Bleu, Noir, Orange
-    char[] couleurs = {'J','R','V','B','N','O'};
+    //Déclaration des variables
+    char[] couleurs = {'J','R','V','B','N','O'};// Jaune, Rouge, Vert, Bleu, Noir, Orange
     char[] combiATrouver = new char[4];
     char[] combiUtilisateur = new char[4];
     int[] tableauPlace = new int[2];
-
     int nombreTours = 0;
     char difficulte;
-
     String nomJoueur1;
     String nomJoueur2;
     String joueur1;
     String joueur2;
-
     boolean bonnesLettres;
 
+    //Début du jeux
+    //Affichage titre et regles
     afficher("titre");
+    afficher("regles");
     afficher("choixNomJoueur1");
     nomJoueur1 = saisie.nextLine();
     afficher("choixNomJoueur2");
@@ -58,8 +62,7 @@ public class MasterMind {
     do {
       afficher("choixUtilisateur", nomJoueur1);
       joueur1 = PasswordField.readPassword(); //affichage sous forme d'étoiles
-      bonnesLettres = joueur1.matches("[RJVBNO]+");
-      // Regex.
+      bonnesLettres = joueur1.matches("[RJVBNO]+");// Regex.
       if (joueur1.length() != 4 || verifLettresDiff(joueur1) == false || bonnesLettres == false) {
         afficher("erreurNbreLettres");
       }
@@ -74,8 +77,7 @@ public class MasterMind {
       do {
         afficher("choixUtilisateur", nomJoueur2);
         joueur2 = saisie.nextLine();
-        bonnesLettres = joueur2.matches("[RJVBNO]+");
-        // Regex.
+        bonnesLettres = joueur2.matches("[RJVBNO]+");// Regex.
         if (joueur2.length() != 4 || verifLettresDiff(joueur2) == false || bonnesLettres == false) {
           afficher("erreurNbreLettres");
         }
@@ -91,16 +93,16 @@ public class MasterMind {
         afficher("gagne");
       }
       nombreTours --;
-
     } while (!Arrays.equals(combiATrouver, combiUtilisateur) && nombreTours != 0);
 
+    //Comparaison des deux suites de lettres pour afficher perdu.
     if (!Arrays.equals(combiATrouver, combiUtilisateur)){
       afficher("perdu");
     }
   } // Fin boucle jeu.
 
+  //Méthode qui affiche et retourne une phrase.
   public static String afficher(String mot) {
-
     Hashtable<String, String> phrase = new Hashtable<String, String>();
     phrase.put("titre", "\n-------------------------\nBienvenue au MasterMind !\n-------------------------\n\n");
     phrase.put("choixDifficulte", "Veuillez choisir une difficulté\n1 : facile.\n2 : moyen.\n3 : difficile.\nChoix : ");
@@ -112,13 +114,13 @@ public class MasterMind {
     phrase.put("gagne", "\n-------------------------\nVous avez gagné !\nSUSHI GOOOOOOOOO\n-------------------------\n\n");
     phrase.put("perdu", "\n-------------------------\nVous avez perdu !\n-------------------------\n");
     phrase.put("sautDeLigne", "\n");
-
     ecritureLente(phrase.get(mot));
     return phrase.get(mot);
   }
 
+  //Méthode qui affiche et retourne le prénom du joueur et une phrase invitant le joueur
+  //à entrer les lettres.
   public static String afficher(String mot, String joueur) {
-
     Hashtable<String, String> phrase = new Hashtable<String, String>();
     phrase.put("choixUtilisateur", joueur + " veuillez entrer 4 lettres différentes (J,R,V,B,N,O) : ");
 
@@ -126,8 +128,8 @@ public class MasterMind {
     return phrase.get(mot);
   }
 
+  //Méthode qui affiche et retourne une phrase qui indique le nombre de tours restant.
   public static String afficher(String mot, int compteur) {
-
     Hashtable<String, String> phrase = new Hashtable<String, String>();
     if (compteur <= 1){
       phrase.put("nombreCoups", "Vous disposez de " + compteur + " coup.");
@@ -135,13 +137,13 @@ public class MasterMind {
     else {
       phrase.put("nombreCoups", "Vous disposez de " + compteur + " coups.");
     }
-
     System.out.println(phrase.get(mot) + "\n");
     return phrase.get(mot);
   }
 
+  //Méthode qui affiche et retourne la phrase de l'ordi disant que les lettres
+  //sont bien ou mal placées.
   public static String afficherBienOuMalPlace(String mot, int bienPlace, int malPlace) {
-
     Hashtable<String, String> phrase = new Hashtable<String, String>();
     if (bienPlace <= 1 && malPlace <= 1){
       phrase.put("reponseBienOuMalPlace", "Vous avez " + bienPlace + " couleur bien placée et " + malPlace + " mal placée." );
@@ -155,11 +157,12 @@ public class MasterMind {
     else {
       phrase.put("reponseBienOuMalPlace", "Vous avez " + bienPlace + " couleurs bien placées et " + malPlace + " mal placées." );
     }
-
     System.out.println(phrase.get(mot));
     return phrase.get(mot);
   }
 
+  //Méthode qui met les lettres rentrées par le joueur en majuscule
+  //et sous forme de tableau.
   public static char[] combinaisonJoueur(String joueur) {
     char[] tableauCouleur = new char[4];
 
@@ -169,6 +172,7 @@ public class MasterMind {
     return tableauCouleur;
   }
 
+  //Méthode qui compare les 2 suites de lettres
   public static int[] comparaisonTableaux(char[] tableau1, char[] tableau2) {
 
     int bienPlace = 0;
@@ -184,16 +188,13 @@ public class MasterMind {
         }
       }
     }
-
     tableauPlace[0] = bienPlace;
     tableauPlace[1] = malPlace;
-
     return tableauPlace;
-
   }
 
+  //Méthode qui vérifie que le joueur rentre bien 4 lettres
   public static boolean verifLettresDiff(String mot) {
-
     boolean isGood = false;
 
     int nombreLettresDiff = (int)mot.chars().filter(Character::isAlphabetic).distinct().count();
@@ -203,6 +204,7 @@ public class MasterMind {
     return isGood;
   }
 
+  //Méthode pour l'écriture des phrases lettre par lettre lentement.
   public static void ecritureLente(String mot){
     for (int i = 0; i < mot.length(); i++){
       System.out.print(mot.charAt(i));
